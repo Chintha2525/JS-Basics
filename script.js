565457c0935c30ad3bfc9dd62e9f542cade501f6
Chintha2525 <dinesharun2525@gmail.com>
2025-01-01 15:49:41 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+function myDate(){
+  // console.log(new Date())
+  const date = new Date()
+  document.getElementById("date").innerHTML = date;
+}
